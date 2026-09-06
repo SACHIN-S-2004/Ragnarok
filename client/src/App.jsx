23b6e8027dff7f831cd643f5cbd11ac6ca337ecb
Sachin Suresh <sachinsuresh9562@gmail.com>
2026-09-06 14:20:58 +0530

    feat: Add Realms page with initial content structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
+import RealmExperience from './pages/RealmExperience';
 
 import './App.css'
 
 function App() {
 
-  return <HomePage />;
+    return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path='/realms' element={<RealmExperience />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
